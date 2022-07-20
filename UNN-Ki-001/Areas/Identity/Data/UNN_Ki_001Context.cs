9d86e1e16e2840c6ceae50086fe65cc785270171
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-20 11:19:31 +0900

    Identityをスキャフォールディング

---
+++ UNN-Ki-001/Areas/Identity/Data/UNN_Ki_001Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNN_Ki_001.Data;
+
+public class UNN_Ki_001Context : IdentityDbContext<IdentityUser>
+{
+    public UNN_Ki_001Context(DbContextOptions<UNN_Ki_001Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
