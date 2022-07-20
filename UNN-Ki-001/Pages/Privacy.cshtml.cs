51f4701e18a9452d24bccbcd6c49ac7d3939aaa0
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-20 11:14:10 +0900

    プロジェクト ファイルを追加します。

---
+++ UNN-Ki-001/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UNN_Ki_001.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
