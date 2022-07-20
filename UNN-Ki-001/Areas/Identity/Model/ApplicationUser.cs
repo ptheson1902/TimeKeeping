87415a87146de0d1becb2c03925e12f981fbe12a
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-20 14:32:34 +0900

    ログイン方法変更

---
+++ UNN-Ki-001/Areas/Identity/Model/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UNN_Ki_001.Areas.Identity.Model
+{
+    public class ApplicationUser: IdentityUser
+    {
+        /// <summary>
+        /// システムログイン時のID。
+        /// 既存のID（GUID）
+        /// </summary>
+        public string? UserId { get; set; }
+    }
+}
