27617dd5d594eecf496ed943c420cad8b6bbcdb5
The Son <ptheson1902@gmail.com>
2022-07-21 17:49:04 +0900

    ログイン間違ってる時のエラーメッセージを設定

---
+++ UNN-Ki-001/Pages/Kinmuhyo/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UNN_Ki_001.Pages.Kinmuhyo
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
