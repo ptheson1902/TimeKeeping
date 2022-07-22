5edf2fad04a02edccbfb86c811ea71ca92e2e7c0
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-22 15:54:03 +0900

    Identity/Account/Manageのデバグ

---
+++ UNN-Ki-001/Areas/Identity/Pages/Account/Manage/ClaimTest.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace UNN_Ki_001.Areas.Identity.Pages.Account.Manage
+{
+    public class ClaimTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Text)]
+            public string RoleName { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            public string ClaimName { get; set; }
+        }
+    }
+}
