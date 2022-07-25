f3fa28c1fc1df794249ed6ee1f26268b16490e28
The Son <ptheson1902@gmail.com>
2022-07-25 17:42:30 +0900

    権限動ぎました。

--- UNN-Ki-001/Areas/Kintai/Pages/BaseSetting.cshtml.cs
+++ UNN-Ki-001/Areas/Kintai/Pages/BaseSetting.cshtml.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 
-namespace UNN_Ki_001.Areas.Kintai
+namespace UNN_Ki_001.Areas.Kintai.Pages
 {
-    public class DakokuModel : PageModel
+    [Authorize(Roles = "Admin")]
+    public class BaseSettingModel : PageModel
     {
         public void OnGet()
         {
