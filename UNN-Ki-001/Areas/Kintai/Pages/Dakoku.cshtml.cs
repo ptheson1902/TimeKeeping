7e45819585df2716c208e87d385aa39039afc4ae
The Son <ptheson1902@gmail.com>
2022-07-25 18:07:27 +0900

    Revert "権限動ぎました。"
    This reverts commit f3fa28c1fc1df794249ed6ee1f26268b16490e28.

--- UNN-Ki-001/Areas/Kintai/Pages/Dakoku.cshtml.cs
+++ UNN-Ki-001/Areas/Kintai/Pages/Dakoku.cshtml.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 
 namespace UNN_Ki_001.Areas.Kintai
 {
-    public class DakokuModel : PageModel
+    [Authorize]
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
