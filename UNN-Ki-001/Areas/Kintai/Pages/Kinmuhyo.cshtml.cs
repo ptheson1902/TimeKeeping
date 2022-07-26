a81fb53ee53523be6cf5e6463843ecc67f6f2679
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-26 09:09:37 +0900

    test

---
+++ UNN-Ki-001/Areas/Kintai/Pages/Kinmuhyo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UNN_Ki_001.Areas.Kintai.Pages
+{
+    public class KinmuhyoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
