16c9ec7314d6cac954c9ecf209dfabf9e8330312
了 松村 <r_matsumura@uninoir.co.jp>
2022-07-28 09:47:55 +0900

    ログインメニューの移行完了

--- UNN-Ki-001/Pages/Account/Login.cshtml.cs
+++ UNN-Ki-001/Pages/Account/Login.cshtml.cs
@@ -15,7 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace UNN_Ki_001.Areas.Identity.Pages.Account
+namespace UNN_Ki_001.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
