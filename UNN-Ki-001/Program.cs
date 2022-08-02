653cb3794ff44b5d86e86f3106dbb8b6c7ee79a3
The Son <ptheson1902@gmail.com>
2022-08-02 18:03:51 +0900

    Downloaded package

--- UNN-Ki-001/Program.cs
+++ UNN-Ki-001/Program.cs
@@ -10,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString)
+);
 
 // Identity‚ð’Ç‰Á
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.Stores.MaxLengthForKeys = 128)
