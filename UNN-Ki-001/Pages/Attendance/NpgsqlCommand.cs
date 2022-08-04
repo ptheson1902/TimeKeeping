a32d06602ddbeb44c6402ed3baf2de595d026d32
Viet Hoang <lehoangjp99@gmail.com>
2022-08-04 09:47:14 +0900

    権限設定済み

---
+++ UNN-Ki-001/Pages/Attendance/NpgsqlCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace UNN_Ki_001.Pages.Attebdabce
+{
+    internal class NpgsqlCommand
+    {
+        public NpgsqlConnection Connection { get; internal set; }
+        public string CommandText { get; internal set; }
+        public CommandType CommandType { get; internal set; }
+
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UNN-Ki-001/Pages/Attendance/index.cshtml.cs
+++ UNN-Ki-001/Pages/Attendance/index.cshtml.cs
@@ -14,25 +14,6 @@ namespace UNN_Ki_001.Pages.Attendance
 
         public void OnGet()
         {
-            try
-            {
-                NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=Kintai;User Id=postgres;Password=123");
-                conn.Open(); 
-                NpgsqlCommand comm = new NpgsqlCommand();
-                comm.Connection = conn;
-                comm.CommandType = CommandType.Text;
-                comm.CommandText = "select * from m_user";
-                NpgsqlDataAdapter nda = new NpgsqlDataAdapter();
-                DataTable dt = new DataTable();
-                nda.Fill(dt);
-                comm.Dispose();
-                conn.Close();
-                Date = "Successfully";
-            }
-            catch (Exception)
-            {
-                Date = "fail";
-            }
         }
     }
 }
