5cd44a45000ecc5bfaed4c02eec41d2d1b37f44b
Viet Hoang <lehoangjp99@gmail.com>
2022-08-05 14:36:06 +0900

    社員検索(80％)

--- UNN-Ki-001/Pages/VariousMaster/shainSearch.cshtml.cs
+++ UNN-Ki-001/Pages/VariousMaster/shainSearch.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using UNN_Ki_001.Data;
 using UNN_Ki_001.Data.Models;
+using PagedList;
 
 namespace UNN_Ki_001.Pages.VariousMaster
 {
@@ -27,13 +28,39 @@ namespace UNN_Ki_001.Pages.VariousMaster
             string name_mei = Request.Form["name_mei"];
             string shozoku_cd = Request.Form["shozoku_cd"];
             string shokushu_cd = Request.Form["shokushu_cd"];
-            string koyokeitai_cd = Request.Form["koyokeitai_cd"];
-            
-            // ŽÐˆõ”Ô†‚ÅŒŸõ
-            /*var no = from a in _context.user
-                     where a.shain_no == shain_no
-                     select a;*/
-            GetData = _context.shain.ToList();
+            string koyokeitai_cd = Request.Form["koyokeitai_cd"];            
+            // ðŒ–³‚µ‚ÅŒŸõ‚·‚é‚±‚ÆB
+            var no = from a in _context.shain
+                     select a;
+
+            if (!string.IsNullOrEmpty(shain_no))
+            {
+                no = no.Where(e => e.shain_no == shain_no);
+            }
+
+            if (!string.IsNullOrEmpty(name_mei))
+            {
+                no = no.Where(e => e.name_mei == name_mei);
+            }
+
+            if (!string.IsNullOrEmpty(shozoku_cd))
+            {
+                no = no.Where(e => e.shozoku_cd == shozoku_cd);
+            }
+
+            if (!string.IsNullOrEmpty(shokushu_cd))
+            {
+                no = no.Where(e => e.shokushu_cd == shokushu_cd);
+            }
+
+            if (!string.IsNullOrEmpty(koyokeitai_cd))
+            {
+                no = no.Where(e => e.koyokeitai_cd == koyokeitai_cd);
+            }
+
+            GetData = await no.ToListAsync();
+
+           
         }
     }
 }
