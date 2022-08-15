a6e6d1500bd61af25386568318138408bb7594e8
了 松村 <r_matsumura@uninoir.co.jp>
2022-08-16 08:44:42 +0900

    所定時間の計算の実装

--- UNN-Ki-001/Data/Models/T_Kinmu.cs
+++ UNN-Ki-001/Data/Models/T_Kinmu.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 
 namespace UNN_Ki_001.Data.Models
 {
@@ -17,12 +18,13 @@ namespace UNN_Ki_001.Data.Models
 
         public override void reload(KintaiDbContext context)
         {
+
             // マスターレコードの取得
             M_Kinmu? m_Kinmu = context.m_kinmus
                 .Where(e => e.KinmuCd.Equals(KinmuCd) && e.KigyoCd.Equals(KigyoCd))
                 .FirstOrDefault();
 
-            // マスターレコードが存在しない場合
+            // マスターレコードが存在しない場合 ///////////////////////////////////////////////////////////
             if (m_Kinmu == null)
             {
                 // 実績時間の計算
@@ -30,38 +32,81 @@ namespace UNN_Ki_001.Data.Models
                 if (KinmuToDate == null) KinmuToDate = DakokuToDate;
                 
                 // マスタレコード無しで集計を行う
-            } 
-            // マスターレコードが存在する場合
+            }
+            // マスターレコードが存在する場合 ///////////////////////////////////////////////////////////
             else
             {
                 // 実績開始時間の計算
-                if (DakokuFrDate != null && KinmuFrDate == null /*←注意:仕様です。*/)
-                {
-                    DateTime DakokuFrDateLocal = ((DateTime)DakokuFrDate).ToLocalTime();
-                    // 丸め処理を行う
-                    DateTime KinmuFrDateTmp = new DateControl((DateTime)DakokuFrDateLocal).MarumeProcess(m_Kinmu.KinmuFrMarumeTm, m_Kinmu.KinmuFrMarumeKbn).Origin;
-
-                    // 刻限の適用
-                    if (m_Kinmu.KinmuFrTm != null && m_Kinmu.KinmuFrKbn != null && m_Kinmu.KinmuFrCtrlFlg != null)
-                    {
-                        DateTime ZissekiFrDate = new DateControl(KinmuDt, m_Kinmu.KinmuFrTm, m_Kinmu.KinmuFrKbn).Origin;
-                        if (ZissekiFrDate > KinmuFrDateTmp)
-                        {
-                            KinmuFrDateTmp = ZissekiFrDate;
-                        }
-                    }
-                    KinmuFrDate = KinmuFrDateTmp.ToUniversalTime();
-                }
+                KinmuFrWrite(m_Kinmu);
 
                 // 実績終了時間の計算
-                if(DakokuToDate != null && KinmuToDate == null /*←仕様です。*/)
-                {
-                    DateTime DakokuToDateLocal = ((DateTime)DakokuToDate).ToLocalTime();
-                    // 丸め処理を行う
-                    KinmuToDate = new DateControl(DakokuToDateLocal).MarumeProcess(m_Kinmu.KinmuToMarumeTm, m_Kinmu.KinmuToMarumeKbn).Origin.ToUniversalTime();
-                }
+                KinmuToWrite(m_Kinmu);
 
                 // マスタレコードを下に集計を行う
+                ShoteiZikan(m_Kinmu);
+            }
+        }
+        private void ShoteiZikan(M_Kinmu masterRecord)
+        {
+            string? kinmuFrTm = masterRecord.KinmuFrTm;
+            string? kinmuToTm = masterRecord.KinmuToTm;
+            if(kinmuFrTm == null || kinmuToTm == null || KinmuFrDate == null || KinmuToDate == null)
+            {
+                return;
+            }
+
+            DateTime tFrDate = ((DateTime)KinmuFrDate).ToUniversalTime();
+            DateTime tToDate = ((DateTime)KinmuToDate).ToUniversalTime();
+            DateTime mFrDate = new DateControl(KinmuDt, kinmuFrTm,  masterRecord.KinmuFrKbn).Origin.ToUniversalTime();
+            DateTime mToDate = new DateControl(KinmuDt, kinmuToTm, masterRecord.KinmuToKbn).Origin.ToUniversalTime();
+            if (mFrDate < tFrDate)
+                mFrDate = tFrDate;
+            if (mToDate > tToDate)
+                mToDate = tToDate;
+
+
+            var res = mToDate - mFrDate;
+            Shotei = (int)res.TotalMinutes;
+        }
+
+        private void KinmuFrWrite(M_Kinmu masterRecord)
+        {
+            // 実績開始時間の計算
+            if (DakokuFrDate != null && KinmuFrDate == null /*←注意:仕様です。*/)
+            {
+                DateTime DakokuFrDateLocal = ((DateTime)DakokuFrDate).ToLocalTime();
+
+                // 丸め処理を行う
+                DateTime result = new DateControl(DakokuFrDateLocal).MarumeProcess(masterRecord.KinmuFrMarumeTm, masterRecord.KinmuFrMarumeKbn).Origin;
+
+                // 刻限の適用
+                if (masterRecord.KinmuFrTm != null && masterRecord.KinmuFrKbn != null && masterRecord.KinmuFrCtrlFlg != null)
+                {
+                    DateTime ZissekiFrDate = new DateControl(KinmuDt, masterRecord.KinmuFrTm, masterRecord.KinmuFrKbn).Origin;
+                    if (ZissekiFrDate > result)
+                    {
+                        result = ZissekiFrDate;
+                    }
+                }
+
+                KinmuFrDate = result.ToUniversalTime();
+            }
+        }
+        private void KinmuToWrite(M_Kinmu masterRecord)
+        {
+            if (DakokuToDate != null && KinmuToDate == null /*←仕様です。*/)
+            {
+                DateTime DakokuToDateLocal = ((DateTime)DakokuToDate).ToLocalTime();
+
+                // 丸め処理を行う
+                KinmuToDate = new DateControl(DakokuToDateLocal).MarumeProcess(masterRecord.KinmuToMarumeTm, masterRecord.KinmuToMarumeKbn).Origin.ToUniversalTime();
+
+                
+                // 整合性を確認（不正なら実績開始時間にあわせる)
+                if(KinmuFrDate != null && ((DateTime)KinmuFrDate).ToUniversalTime() > KinmuToDate)
+                {
+                    KinmuToDate = ((DateTime)KinmuFrDate).ToUniversalTime();
+                }
             }
         }
 
