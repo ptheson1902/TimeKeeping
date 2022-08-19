c0dfc9f7daf2f2a4fed8f80d637fad9d1c32ace7
The Son <ptheson1902@gmail.com>
2022-08-19 12:42:00 +0900

    GetData作成

---
+++ UNN-Ki-001/Pages/Attendance/GetData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UNN_Ki_001.Pages.Attendance
+{
+    public class GetDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
