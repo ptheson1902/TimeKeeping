1c3b61b7b10c67540d8a711b164a7e5c8cc1bb7e
了 松村 <r_matsumura@uninoir.co.jp>
2022-08-24 11:17:38 +0900

    勤務表アップデート

--- UNN-Ki-001/Data/Models/T_Kinmu.cs
+++ UNN-Ki-001/Data/Models/T_Kinmu.cs
@@ -297,30 +297,6 @@ namespace UNN_Ki_001.Data.Models
         [Column("kinmu_to_date")]
         public DateTime? KinmuToDate { get; private set; }
 
-        [Column("dakoku_fr_dt")]
-        public string? DakokuFrDt { get; private set; }
-
-        [Column("dakoku_fr_tm")]
-        public string? DakokuFrTm { get; private set; }
-
-        [Column("dakoku_to_dt")]
-        public string? DakokuToDt { get; private set; }
-
-        [Column("dakoku_to_tm")]
-        public string? DakokuToTm { get; private set; }
-
-        [Column("kinmu_fr_dt")]
-        public string? KinmuFrDt { get; private set; }
-
-        [Column("kinmu_fr_tm")]
-        public string? KinmuFrTm { get; private set; }
-
-        [Column("kinmu_to_dt")]
-        public string? KinmuToDt { get; private set; }
-
-        [Column("kinmu_to_tm")]
-        public string? KinmuToTm { get; private set; }
-
         [Column("shotei")]
         public int? Shotei { get; set; }
 
--- UNN-Ki-001/Pages/Attendance/Record/Index.cshtml.cs
+++ UNN-Ki-001/Pages/Attendance/Record/Index.cshtml.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Globalization;
 using UNN_Ki_001.Data;
 using UNN_Ki_001.Data.Models;
 
@@ -11,6 +13,8 @@ namespace UNN_Ki_001.Pages.Attendance.Record
     [Authorize(Policy = "Rookie")]
     public class IndexModel : BasePageModel
     {
+        public List<Kinmuhyo> DataList = new List<Kinmuhyo>();
+
         public IndexModel(KintaiDbContext kintaiDbContext, UserManager<AppUser> userManager) : base(kintaiDbContext, userManager)
         {
         }
@@ -28,7 +32,7 @@ namespace UNN_Ki_001.Pages.Attendance.Record
                 var index = temp.CurrentIndex;
                 if(length == 0 || index < 0 || index >= length)
                 {
-                    return NotFound();
+                    throw new Exception("検索対象の情報が破損しています。");
                 }
             }
 
@@ -37,21 +41,85 @@ namespace UNN_Ki_001.Pages.Attendance.Record
 
             // カレントインデックスの情報を取り出し、ビューに渡します。
             ShainSearchRecord target = targetList.GetCurrent();
-            ViewData["target"] = target;
+            // 閲覧月のデータを取得
+            DateTime currentMonth = target.GetCurrentDate();
+            // 企業コードで設定をSELECT
+            var setting = _kintaiDbContext.mSettings
+                .Where(e => e.KigyoCd.Equals(target.KigyoCd))
+                .FirstOrDefault();
+            // 設定から締め日を抽出（初期値: 99(末日))
+            int shimebi = (setting == null || setting.ShimeDt == null) 
+                ? 99 : (int)setting.ShimeDt;
 
-
-            // 企業コードで設定を読み込み
-            
-
-            // 開始日を取得
-
-            // 終了日を取得
+            // 締め日翌日から締め日までのKinmuDt一覧を取得
+            DayList calender = new DayList(currentMonth, shimebi);
+            var kinmuDtList = calender.KinmuDtList;
 
             // 勤務データを取り込み
-            
-            // 存在しないデータを埋める
+            var tempList = _kintaiDbContext.t_kinmus
+                .Include(e => e.MKinmu)
+                .Where(e => e.KigyoCd!.Equals(target.KigyoCd)
+                    && e.ShainNo!.Equals(target.ShainNo)
+                    && kinmuDtList.Contains(e.KinmuDt!))
+                .OrderBy(e => e.KinmuDt)
+                .ToList();
 
-            // Viewに渡す
+            // 存在しないデータも埋めて出力用の整形済みデータを作成する
+            CultureInfo Japanese = new CultureInfo("ja-JP");
+            foreach (var kinmuDt in kinmuDtList)
+            {
+                var data = new Kinmuhyo();
+                var day = DateTime.ParseExact(kinmuDt, "yyyyMMdd", null); // 変数kinmuDtがyyyyMMddのフォーマットであることはDayListクラスによって保証されている
+                data.Day = day.ToString("MM月dd日(ddd)", Japanese);
+
+                // 勤務レコードを取得
+                var kinmu = tempList
+                    .Where(e => e.KinmuDt!.Equals(kinmuDt))
+                    .FirstOrDefault();
+
+                // nullならcontinue(初期値はコンストラクタで挿入済み）
+                if (kinmu == null)
+                {
+                    DataList.Add(data);
+                    continue;
+                }
+
+                    // 勤務予定
+                    if (kinmu.MKinmu != null && kinmu.MKinmu.KinmuNm != null)
+                    data.Yote = kinmu.MKinmu.KinmuNm;
+
+                // 時刻フォーマットを宣言
+                string timeFormat = "HH:mm";
+
+                // 時刻
+                if (kinmu.DakokuFrDate != null)
+                    data.DakokuStart = ((DateTime)kinmu.DakokuFrDate).ToString(timeFormat);
+                if(kinmu.DakokuToDate != null)
+                    data.DakokuEnd = ((DateTime)kinmu.DakokuToDate).ToString(timeFormat);
+                if (kinmu.KinmuFrDate != null)
+                    data.KinmuStart = ((DateTime)kinmu.KinmuFrDate).ToString(timeFormat);
+                if (kinmu.KinmuToDate != null)
+                    data.KinmuEnd = ((DateTime)kinmu.KinmuToDate).ToString(timeFormat);
+
+                // 休憩
+                if (kinmu.Kyukei != null)
+                    data.Kyukei = MinutesToString((int)kinmu.Kyukei);
+
+                //総労働
+                if (kinmu.Sorodo != null)
+                    data.Sorodo = MinutesToString((int)kinmu.Sorodo);
+
+                // 控除
+                if (kinmu.Kojo != null)
+                    data.Kojo = MinutesToString((int)kinmu.Kojo);
+
+                // 備考
+                if (kinmu.Biko != null)
+                    data.Biko = kinmu.Biko;
+
+                // 作成完了・リストに追加
+                DataList.Add(data);
+            }
 
             return Page();
         }
@@ -71,22 +139,68 @@ namespace UNN_Ki_001.Pages.Attendance.Record
 
         }
         */
+
+        public string MinutesToString(int val)
+        {
+            int hour = val / 60;
+            int min = val % 60;
+            return hour.ToString("00") + ":" + min.ToString("00");
+        }
     }
+
+    public class Kinmuhyo
+    {
+        public Kinmuhyo()
+        {
+            Day = DakokuStart
+                = DakokuEnd
+                = KinmuStart
+                = KinmuEnd
+                = Kyukei
+                = Sorodo
+                = Kojo
+                = Biko
+                = "";
+            Yote = "未設定";
+        }
+
+        public string Day { get; set; }
+        public string Yote { get; set; }
+        public string DakokuStart { get; set; }
+        public string DakokuEnd { get; set; }
+        public string KinmuStart { get; set; }
+        public string KinmuEnd { get; set; }
+        public string Kyukei { get; set; }
+        public string Kojo { get; set; }
+        public string Sorodo { get; set; }
+        public string Biko { get; set; }
+    }
+
 
     /// <summary>
     /// 日付を指定するとその月のカレンダーを生成するオブジェクトです。
     /// </summary>
-    class Kinmuhyo
+    class DayList
     {
-        public DateTime StartDate;
-        public DateTime EndDate;
+        public readonly DateTime StartDate;
+        public readonly DateTime EndDate;
+        public readonly int Count;
+        public readonly List<string> KinmuDtList; 
 
-        public Kinmuhyo(DateTime date, int shimebi)
+        public DayList(DateTime date, int shimebi)
         {
             // 締め日に99を指定された場合は、月末を意味するので割り込み処理
             if (shimebi == 99)
             {
                 shimebi = 0;
+            }
+            // C#のDateTimeは0以下の日付に対応できないため、その場合は後から差分を減算することで対応します。
+            // よって、０以下の場合は1との差を計測します
+            int mainasu = 0;
+            if (shimebi < 1)
+            {
+                mainasu = shimebi - 1;
+                shimebi = 1;
             }
 
             // 開始日を計算
@@ -101,9 +215,27 @@ namespace UNN_Ki_001.Pages.Attendance.Record
 
             // 日付型を作成
             StartDate = new DateTime(startYear, startMonth, shimebi);
-            StartDate = StartDate.AddDays(1);
+            StartDate = StartDate.AddDays(1 + mainasu);
             EndDate = new DateTime(endYear, endMonth, shimebi);
+            EndDate = EndDate.AddDays(mainasu);
 
+            // 日数を取得して格納
+            Count = (EndDate - StartDate).Days + 1;
+
+            // カレンダー形式で勤務記録を取得
+            KinmuDtList = GetKinmuDtList(StartDate, Count);
+        }
+
+        public static List<string> GetKinmuDtList(DateTime startDay, int length)
+        {
+            // kinmuDtのリストを作成
+            List<String> kinmuDtList = new List<string>();
+            for(int i=0; i<length; i++)
+            {
+                kinmuDtList.Add(startDay.AddDays(i).ToString("yyyyMMdd"));
+            }
+
+            return kinmuDtList;
         }
     }
 }
