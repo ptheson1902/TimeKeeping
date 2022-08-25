4f603739182262df5bf5ad95e39fb73caceee2fb
Viet Hoang <lehoangjp99@gmail.com>
2022-08-25 11:15:26 +0900

    kinmu master

--- UNN-Ki-001/Pages/VariousMaster/Kinmu.cshtml.cs
+++ UNN-Ki-001/Pages/VariousMaster/Kinmu.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using UNN_Ki_001.Data;
@@ -5,25 +6,42 @@ using UNN_Ki_001.Data.Models;
 
 namespace UNN_Ki_001.Pages.VariousMaster
 {
-    public class KinmuModel : PageModel
+    public class KinmuModel : BasePageModel
     {
         private readonly KintaiDbContext _context;
         private readonly ApplicationDbContext context1;
         public List<Display> Data = new List<Display>();
+
+        public KinmuModel(KintaiDbContext kintaiDbContext, UserManager<AppUser> userManager) : base(kintaiDbContext, userManager)
+        {
+        }
+
+        public List<M_Kinmu> Kinmu { get; set; }
         public string? Kinmu_cd { get; set; }
         public string? Kinmu_nm { get; set; }
         public string? Valid_flg { get; set; }
         public string? Kinmu_bunrui { get; set; }
-        public KinmuModel(UNN_Ki_001.Data.KintaiDbContext context, ApplicationDbContext application)
-        {
-            _context = context;
-            context1 = application;
-        }
+        public string? Message { get; set; }
         public void OnGet()
         {
-        }
+            var shain = GetCurrentUserShainAsync().Result;
+            Kinmu = (from e in _kintaiDbContext.m_kinmus
+                     where e.KigyoCd!.Equals(shain!.KigyoCd)
+                     orderby e.KinmuBunrui                   
+                     select e).ToList();
+            
+            
+
+        }                                                     
         public void OnPost()
         {
+            var shain = GetCurrentUserShainAsync().Result;
+            Kinmu = 
+                (from e in _kintaiDbContext.m_kinmus
+                     where e.KigyoCd!.Equals(shain!.KigyoCd)
+                     orderby e.KinmuBunrui                   
+                     select e).ToList();
+
 
             var action = Request.Form["action"];
             switch (action)
@@ -65,7 +83,7 @@ namespace UNN_Ki_001.Pages.VariousMaster
             Kinmu_nm = Request.Form["kinmu_nm"];
             Kinmu_bunrui = Request.Form["kinmu_bunrui"];
             Valid_flg = Request.Form["valid_flg"];
-            var no = from m_kinmu in _context.m_kinmus
+            var no = from m_kinmu in _kintaiDbContext.m_kinmus
                      orderby m_kinmu.KinmuCd
                      select new { m_kinmu.KinmuCd, m_kinmu.KinmuNm, m_kinmu.ValidFlg , m_kinmu.KinmuBunrui ,m_kinmu.KinmuFrTm , m_kinmu.KinmuToTm , m_kinmu.Kyukei1FrTm , m_kinmu.Kyukei1ToTm};
             // èåèÇ…ÇÊÇÈåüçıÇ∑ÇÈÇ±Ç∆(valueÅÅnullÇÕåüçıèåèÇ…Ç»ÇÁÇ»Ç¢Ç±Ç∆ÅB)
@@ -106,6 +124,7 @@ namespace UNN_Ki_001.Pages.VariousMaster
         }
         private void Register()
         {
+            string kigyocd1 = "C001";
             string kinmu_cd1 = Request.Form["kinmu_cd1"];
             string kinmu_nm1 = Request.Form["kinmu_nm1"];
             string kinmu_bunrui1 = Request.Form["kinmu_bunrui1"];
@@ -115,6 +134,19 @@ namespace UNN_Ki_001.Pages.VariousMaster
             string kyuke1_to_tm1 = Request.Form["kyuke1_to_tm1"];
             string kyukei_auto_flg1 = Request.Form["kyukei_auto_flg1"];
             string kinmu_fr_ctrl_flg1 = Request.Form["kinmu_fr_ctrl_flg1"];
+
+            M_Kinmu mk = new M_Kinmu( kigyocd1 , kinmu_cd1, kinmu_nm1 , kinmu_bunrui1);
+            _context.m_kinmus.Add(mk);
+            var a = _context.SaveChanges();
+            if (a < 0)
+            {
+                Message = "ìoò^Ç≈Ç´Ç‹ÇπÇÒÇ≈ÇµÇΩÅB";
+            }
+            else
+            {
+                Message = "ìoò^Ç≈Ç´Ç‹ÇµÇΩÅB";
+            }
         }
+
     }
 }
